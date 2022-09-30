2443923fba5dac5631c0651e3232d1a38e9cb7d7
Cole Amlong <coleamlong@gmail.com>
2022-09-30 18:26:26 -0500

    About page static information

---
+++ front-end/src/static/ProjectInfo.jsx
@@ -0,0 +1,72 @@
+const toolInfo = [
+  {
+		title: "React",
+		image: ReactLogo,
+		description: "JavaScript library for front-end development",
+		link: "https://reactjs.org/",
+	},
+	{
+		title: "Postman",
+		image: PostmanLogo,
+		description: "Tool for designing and testing APIs",
+		link: "https://postman.com/",
+	},
+	{
+		title: "AWS",
+		image: AWSLogo,
+		description: "Cloud hosting platform",
+		link: "https://aws.amazon.com/",
+	},
+	{
+		title: "Docker",
+		image: DockerLogo,
+		description:
+			"Containerization tool for consistent runtime environments",
+		link: "https://docker.com/",
+	},
+	{
+		title: "GitLab",
+		image: GitLabLogo,
+		description: "Git repository and CI/CD platform",
+		link: "https://gitlab.com/",
+	},
+	{
+		title: "Discord",
+		image: DiscordLogo,
+		description: "Team communication platform",
+		link: "https://discord.com/",
+	},
+	{
+		title: "Namecheap",
+		image: NamecheapLogo,
+		description: "Domain name registrar",
+		link: "https://namecheap.com/",
+	},
+]
+
+const apiInfo = [
+  {
+		title: "RoadGoat API",
+		image: RoadGoatLogo,
+		description: "Geological API with information on cities",
+		link: "https://www.roadgoat.com/",
+	},
+  {
+		title: "Google Maps API",
+		image: GoogleMapsLogo,
+		description: "Geological API with route times and address information",
+		link: "https://developers.google.com/maps",
+	},
+  {
+		title: "ZipRecruiter API",
+		image: ZipRecruiterLogo,
+		description: "Real Estate API",
+		link: "https://www.ziprecruiter.com/zipsearch",
+	},
+  {
+		title: "Apartments.com API",
+		image: ApartmentsComLogo,
+		description: "Get addresses and prices of apartments on the market in a city",
+		link: "https://api.apartments.com/v1",
+	},
+]
---
+++ front-end/src/static/TeamInfo.jsx
@@ -0,0 +1,61 @@
+import Avatar from "../assets/placeholder/avatar.png"
+
+// inspired by TexasVotes project. https://gitlab.com/forbesye/fitsbits/-/blob/master/front-end/src/views/About/AboutInfo.js
+
+const teamInfo = [
+  {
+    name: "Cole Amlong",
+    gitlab_username: "coleamlong",
+    email: "coleamlong@gmail.com",
+    image: Avatar,
+    role: "Front-end",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ullamcorper purus interdum orci auctor, id venenatis sem congue. Morbi condimentum mi non risus bibendum, accumsan tincidunt lacus suscipit.",
+    commits: 0,
+    issues: 0,
+    unit_tests: 0,
+  },
+  {
+    name: "Faiza Rahman",
+    gitlab_username: "faiza2002",
+    email: "---",
+    image: Avatar,
+    role: "---",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ullamcorper purus interdum orci auctor, id venenatis sem congue. Morbi condimentum mi non risus bibendum, accumsan tincidunt lacus suscipit.",
+    commits: 0,
+    issues: 0,
+    unit_tests: 0,
+  },
+  {
+    name: "Mikala Jaramillo",
+    gitlab_username: "Mikalajj",
+    email: "---",
+    image: Avatar,
+    role: "---",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ullamcorper purus interdum orci auctor, id venenatis sem congue. Morbi condimentum mi non risus bibendum, accumsan tincidunt lacus suscipit.",
+    commits: 0,
+    issues: 0,
+    unit_tests: 0,
+  },
+  {
+    name: "Sarthak Sirotiya",
+    gitlab_username: "sarthaksirotiya",
+    email: "---",
+    image: Avatar,
+    role: "---",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ullamcorper purus interdum orci auctor, id venenatis sem congue. Morbi condimentum mi non risus bibendum, accumsan tincidunt lacus suscipit.",
+    commits: 0,
+    issues: 0,
+    unit_tests: 0,
+  },
+  {
+    name: "Thomas Langford",
+    gitlab_username: "thomas_langford",
+    email: "---",
+    image: Avatar,
+    role: "---",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ullamcorper purus interdum orci auctor, id venenatis sem congue. Morbi condimentum mi non risus bibendum, accumsan tincidunt lacus suscipit.",
+    commits: 0,
+    issues: 0,
+    unit_tests: 0,
+  },
+]
