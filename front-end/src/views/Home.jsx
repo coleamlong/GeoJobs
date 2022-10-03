397bceb9f473527ce3d5111dfbdde8500aa3656c
Sarthak Sirotiya <sarthakssi2@gmail.com>
2022-10-03 03:14:56 -0500

    Added background image to home page, resolves #20

--- front-end/src/views/Home.jsx
+++ front-end/src/views/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Image from 'react-bootstrap/Image'
+import Background from '../assets/placeholder/background.jpeg'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-    
+    <Image style={{height:'auto', width: '100%'}}
+      src={Background}
+    />
   )
 }
 
