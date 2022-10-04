924bb228929bf0b7fb4f9e554da3818d2f2f1da1
Cole Amlong <coleamlong@gmail.com>
2022-10-03 19:55:36 -0500

    changed to dark theme

--- front-end/src/views/About.jsx
+++ front-end/src/views/About.jsx
@@ -100,7 +100,7 @@ const About = () => {
           GeoJobs is a one-stop shop for all of you career needs! Browse open job listings, hunt for a nearby apartment, and explore interesting cities all over the country. <br /><br /> To get started, simply navigate to one of the three categories and start browsing!
         </p>
       </Container>
-      <Container className='p-4 bg-primary text-light'>
+      <Container className='p-4 bg-dark text-light'>
         <h1 className='d-flex justify-content-center p-4 '>Putting The Pieces Together</h1>
         <p className='mx-auto'>
           GeoJobs integrates data from many sources, including job boards, human geography data, and apartment listings, to present you with useful connections which help you plan a big move in your career or a have a quick look around the city you are already in.
@@ -130,7 +130,7 @@ const About = () => {
           )
         }
       </Container>
-      <Container className='p-4 bg-primary text-light'>
+      <Container className='p-4 bg-dark text-light'>
         <h1 className='d-flex justify-content-center p-4'>Total Repsitory Stats</h1>
         <Row className='p-4'>
           <Col className='d-flex justify-content-center'><h2>Total Commits: { totalCommits }</h2></Col>
