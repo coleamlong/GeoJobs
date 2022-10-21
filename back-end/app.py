c7cc6ec90283a17616f2ba0ab92c2943443caf5a
UTSHPE <thomaslangford0123@gmail.com>
2022-10-21 16:20:11 -0500

    Added tags city request and implemented pagination

--- back-end/app.py
+++ back-end/app.py
@@ -1,9 +1,10 @@
 from flask import jsonify, request, Response
 from models import app, db, Job, Apartment, City
-from schema import job_schema, city_schema, apartment_schema
+from schema import job_schema, city_schema, apartment_schema, tag_schema
 from sqlalchemy.sql import text, column
 import json
 
+DEFAULT_PAGE_SIZE = 20
 
 @app.route("/")
 def home():
@@ -17,8 +18,12 @@ def home():
 
 @app.route("/cities")
 def get_cities():
+    # get args
+    page = request.args.get("page", type=int)
+    perPage = request.args.get("perPage", type=int)
     query = db.session.query(City)
     count = query.count()
+    query = paginate(query, page, perPage)
     result = city_schema.dump(query, many=True)
     return jsonify(
         {
@@ -64,7 +69,12 @@ def get_city(r_id):
         result = city_schema.dump(query, many=True)[0]
     except IndexError:
         return return_error(f"Invalid city ID: {r_id}")
-    return jsonify(result)
+    city = query.first()
+    city_tags = tag_schema.dump(city.tags, many=True)
+    return jsonify({
+        "data": result,
+        "tags": city_tags
+    })
 
 @app.route("/jobs/<int:r_id>")
 def get_job(r_id):
@@ -91,6 +101,14 @@ def return_error(msg):
     resp = Response(json.dumps({"error": msg}), mimetype="application/json")
     resp.error_code = 404
     return resp
+
+"""
+Returns a paginated query according the page number and number per page
+"""
+def paginate(query, page_num, num_per_page):
+    if num_per_page is None:
+        num_per_page = DEFAULT_PAGE_SIZE
+    return query.paginate(page=page_num, per_page=num_per_page, error_out=False).items
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
--- back-end/schema.py
+++ back-end/schema.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from models import Job, Apartment, City
+from models import Job, Apartment, City, Tag
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 ma = Marshmallow()
@@ -17,6 +17,11 @@ class CitySchema(SQLAlchemyAutoSchema):
     class Meta:
         model = City
 
+class TagSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Tag
+
 job_schema = JobSchema()
 apartment_schema = ApartmentSchema()
 city_schema = CitySchema()
+tag_schema = TagSchema()
