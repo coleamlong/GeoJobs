523b3ff880643b0e8688336e188ca4c289826d42
Cole Amlong <coleamlong@gmail.com>
2022-10-26 12:33:01 -0500

    Pagination on Jobs and Apartments

--- front-end/src/views/Apartments.jsx
+++ front-end/src/views/Apartments.jsx
@@ -5,6 +5,7 @@ import Spinner from "react-bootstrap/Spinner";
 import Container from "react-bootstrap/Container";
 import axios from "axios";
 import ApartmentCard from "../components/Cards/ApartmentCard";
+import Pagination from "react-bootstrap/Pagination";
 
 const client = axios.create({
   baseURL: "https://api.geojobs.me/",
@@ -13,12 +14,19 @@ const client = axios.create({
 const Apartments = () => {
   const [apartments, setApartments] = useState([]);
   const [loaded, setLoaded] = useState(false);
+  const [activePage, setActivePage] = useState(1);
+
+  function handleClick(number) {
+    console.log("Clicked page ", number);
+    setActivePage(number);
+    setLoaded(false);
+  }
 
   useEffect(() => {
     const fetchApartments = async () => {
-      if (apartments === undefined || apartments.length === 0) {
+      if (!loaded) {
         await client
-          .get("apartments")
+          .get(`apartments?page=${activePage}&perPage=20`)
           .then((response) => {
             setApartments(response.data);
             console.log(response.data);
@@ -28,11 +36,52 @@ const Apartments = () => {
       }
     };
     fetchApartments();
-  }, [apartments]);
+  }, [apartments, loaded]);
+
+  let numPages = 500 / 20;
+  let items = [];
+  for (let number = activePage - 2; number <= activePage + 2; number++) {
+    if (number > 0 && number <= numPages) {
+      items.push(
+        <Pagination.Item
+          key={number}
+          onClick={() => handleClick(number)}
+          active={number === activePage}
+        >
+          {number}
+        </Pagination.Item>
+      );
+    }
+  }
 
   return (
     <Container>
       <h1 className="p-5 text-center">Apartments</h1>
+      <Pagination className="justify-content-center">
+        {activePage > 3 && (
+          <Pagination.Item
+            first
+            key={1}
+            onClick={() => handleClick(1)}
+            active={1 === activePage}
+          >
+            1
+          </Pagination.Item>
+        )}
+        {activePage > 4 && <Pagination.Ellipsis />}
+        {items}
+        {activePage < numPages - 3 && <Pagination.Ellipsis />}
+        {activePage < numPages - 2 && (
+          <Pagination.Item
+            last
+            key={numPages}
+            onClick={() => handleClick(numPages)}
+            active={numPages === activePage}
+          >
+            {numPages}
+          </Pagination.Item>
+        )}
+      </Pagination>
       <Row md={4} className="d-flex g-4 p-4 justify-content-center">
         {loaded ? (
           apartments["data"].map((apartment) => {
--- front-end/src/views/Jobs.jsx
+++ front-end/src/views/Jobs.jsx
@@ -5,6 +5,7 @@ import Spinner from "react-bootstrap/Spinner";
 import Container from "react-bootstrap/Container";
 import axios from "axios";
 import JobCard from "../components/Cards/JobCard";
+import Pagination from "react-bootstrap/Pagination";
 
 const client = axios.create({
   baseURL: "https://api.geojobs.me/",
@@ -13,12 +14,19 @@ const client = axios.create({
 const Jobs = () => {
   const [jobs, setJobs] = useState([]);
   const [loaded, setLoaded] = useState(false);
+  const [activePage, setActivePage] = useState(1);
+
+  function handleClick(number) {
+    console.log("Clicked page ", number);
+    setActivePage(number);
+    setLoaded(false);
+  }
 
   useEffect(() => {
     const fetchJobs = async () => {
-      if (jobs === undefined || jobs.length === 0) {
+      if (!loaded) {
         await client
-          .get("jobs?")
+          .get(`jobs?page=${activePage}&perPage=20`)
           .then((response) => {
             setJobs(response.data);
           })
@@ -27,11 +35,52 @@ const Jobs = () => {
       }
     };
     fetchJobs();
-  }, [jobs]);
+  }, [jobs, loaded]);
+
+  let numPages = 1000 / 20;
+  let items = [];
+  for (let number = activePage - 2; number <= activePage + 2; number++) {
+    if (number > 0 && number <= numPages) {
+      items.push(
+        <Pagination.Item
+          key={number}
+          onClick={() => handleClick(number)}
+          active={number === activePage}
+        >
+          {number}
+        </Pagination.Item>
+      );
+    }
+  }
 
   return (
     <Container>
       <h1 className="p-5 text-center">Jobs</h1>
+      <Pagination className="justify-content-center">
+        {activePage > 3 && (
+          <Pagination.Item
+            first
+            key={1}
+            onClick={() => handleClick(1)}
+            active={1 === activePage}
+          >
+            1
+          </Pagination.Item>
+        )}
+        {activePage > 4 && <Pagination.Ellipsis />}
+        {items}
+        {activePage < numPages - 3 && <Pagination.Ellipsis />}
+        {activePage < numPages - 2 && (
+          <Pagination.Item
+            last
+            key={numPages}
+            onClick={() => handleClick(numPages)}
+            active={numPages === activePage}
+          >
+            {numPages}
+          </Pagination.Item>
+        )}
+      </Pagination>
       <Row md={4} className="d-flex g-4 p-4 justify-content-center">
         {loaded ? (
           jobs["data"].map((job) => {
