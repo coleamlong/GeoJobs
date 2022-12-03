a3c4ad4160e9eac040139002f561026ee5cbf9f8
faizarahman <faiza2002@gmail.com>
2022-12-03 15:20:30 -0600

    changed color of cards

--- front-end/src/components/Cards/ApartmentCard.jsx
+++ front-end/src/components/Cards/ApartmentCard.jsx
@@ -26,7 +26,7 @@ const ApartmentCard = (props) => {
 
   return (
     <Card
-    style= {{backgroundColor: '#f8edeb'}}>
+    style= {{backgroundColor: '#ffffff'}}>
       <Card.Img
         style={{
           height: "45%",
