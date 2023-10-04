dedef5052cd9c11023c17a25fbc616db27d940af
Cole Amlong <coleamlong@gmail.com>
2023-10-04 18:41:43 -0500

    switched to hashRouter

--- front-end/src/RouteSwitch.jsx
+++ front-end/src/RouteSwitch.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import About from "./views/About";
 import Apartment from "./views/Apartment";
@@ -8,27 +8,27 @@ import City from "./views/City";
 import Home from "./views/Home";
 import Job from "./views/Job";
 import Jobs from "./views/Jobs";
-import Search from "./views/Search"
-import Visualizations from "./views/Visualizations"
+import Search from "./views/Search";
+import Visualizations from "./views/Visualizations";
 
 const RouteSwitch = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/cities" element={<Cities />} />
-        <Route path="/cities/:id" element={<City />} />
-        <Route path="apartments" element={<Apartments />} />
-        <Route path="/apartment/:id" element={<Apartment />} />
-        <Route path="/jobs" element={<Jobs />} />
-        <Route path="/job/:id" element={<Job />} />
-        <Route path="visualizations" element={<Visualizations />} />
-        {/* site-wide search */}
-        <Route path="/search/:query" element={<Search />} /> 
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/cities" element={<Cities />} />
+                <Route path="/cities/:id" element={<City />} />
+                <Route path="apartments" element={<Apartments />} />
+                <Route path="/apartment/:id" element={<Apartment />} />
+                <Route path="/jobs" element={<Jobs />} />
+                <Route path="/job/:id" element={<Job />} />
+                <Route path="visualizations" element={<Visualizations />} />
+                {/* site-wide search */}
+                <Route path="/search/:query" element={<Search />} />
+            </Routes>
+        </HashRouter>
+    );
 };
 
 export default RouteSwitch;
